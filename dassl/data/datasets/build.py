77eba8e1a433e61a108b9252ed9bf78d89b8774d
Tong Wang <leowangtong@gmail.com>
2025-06-02 00:05:23 +0800

    Delete dassl directory

--- dassl/data/datasets/build.py
+++
@@ -1,9 +0,0 @@
-from dassl.utils import Registry, check_availability
-
-DATASET_REGISTRY = Registry("DATASET")
-
-
-def build_dataset(cfg):
-    avai_datasets = DATASET_REGISTRY.registered_names()
-    check_availability(cfg.DATASET.NAME, avai_datasets)
-    return DATASET_REGISTRY.get(cfg.DATASET.NAME)(cfg)
--- dassl/engine/build.py
+++
@@ -1,9 +0,0 @@
-from dassl.utils import Registry, check_availability
-
-TRAINER_REGISTRY = Registry("TRAINER")
-
-
-def build_trainer(cfg):
-    avai_trainers = TRAINER_REGISTRY.registered_names()
-    check_availability(cfg.TRAINER.NAME, avai_trainers)
-    return TRAINER_REGISTRY.get(cfg.TRAINER.NAME)(cfg)
